a05edf7dee1a1a9248bbe6fadb7b85d4ca6a2ab7
Prison <golubev.artur.1986@gmail.com>
2022-08-31 20:42:46 +0300

    feat: implement search box and header

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,13 @@
 // import { isMobile } from "./functions.js";
 // import { formsModules } from "./forms/forms.js";
 
+let searchForm = document.querySelector('.header .search-form');
+
+document.querySelector('#search-btn').onclick = () => {
+	searchForm.classList.toggle('_active');
+	console.log(searchForm);
+}
+
+window.onscroll = () => {
+	searchForm.classList.remove('_active');
+}
